1efd6feaca851a674eae777e9734d48f232a33a4
crtmneric <s3rkanodemir@gmail.com>
2016-09-17 11:25:54 +0300

    First Release
    v1.0

---
+++ TetrisTCP/TetrisTCP/frmMain.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace TetrisTCP
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.tmrMovements = new System.Windows.Forms.Timer(this.components);
+            this.pctBall = new System.Windows.Forms.PictureBox();
+            this.pctPlayer2 = new System.Windows.Forms.PictureBox();
+            this.pctPlayer1 = new System.Windows.Forms.PictureBox();
+            this.pctLeftWall = new System.Windows.Forms.PictureBox();
+            this.pctRigtWall = new System.Windows.Forms.PictureBox();
+            this.tmrStart = new System.Windows.Forms.Timer(this.components);
+            this.tmrBall = new System.Windows.Forms.Timer(this.components);
+            this.lblKeys = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLeftWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRigtWall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 43.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblInfo.Location = new System.Drawing.Point(71, 264);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 83);
+            this.lblInfo.TabIndex = 3;
+            // 
+            // tmrMovements
+            // 
+            this.tmrMovements.Tick += new System.EventHandler(this.tmrMovements_Tick);
+            // 
+            // pctBall
+            // 
+            this.pctBall.BackgroundImage = global::TetrisTCP.Properties.Resources.top;
+            this.pctBall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctBall.Location = new System.Drawing.Point(676, 415);
+            this.pctBall.Name = "pctBall";
+            this.pctBall.Size = new System.Drawing.Size(48, 34);
+            this.pctBall.TabIndex = 2;
+            this.pctBall.TabStop = false;
+            // 
+            // pctPlayer2
+            // 
+            this.pctPlayer2.BackgroundImage = global::TetrisTCP.Properties.Resources.cubuk;
+            this.pctPlayer2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctPlayer2.Location = new System.Drawing.Point(619, 787);
+            this.pctPlayer2.Name = "pctPlayer2";
+            this.pctPlayer2.Size = new System.Drawing.Size(163, 36);
+            this.pctPlayer2.TabIndex = 1;
+            this.pctPlayer2.TabStop = false;
+            // 
+            // pctPlayer1
+            // 
+            this.pctPlayer1.BackgroundImage = global::TetrisTCP.Properties.Resources.cubuk;
+            this.pctPlayer1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctPlayer1.Location = new System.Drawing.Point(619, -2);
+            this.pctPlayer1.Name = "pctPlayer1";
+            this.pctPlayer1.Size = new System.Drawing.Size(163, 36);
+            this.pctPlayer1.TabIndex = 0;
+            this.pctPlayer1.TabStop = false;
+            // 
+            // pctLeftWall
+            // 
+            this.pctLeftWall.BackgroundImage = global::TetrisTCP.Properties.Resources.duvar;
+            this.pctLeftWall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctLeftWall.Location = new System.Drawing.Point(1, -2);
+            this.pctLeftWall.Name = "pctLeftWall";
+            this.pctLeftWall.Size = new System.Drawing.Size(24, 825);
+            this.pctLeftWall.TabIndex = 4;
+            this.pctLeftWall.TabStop = false;
+            // 
+            // pctRigtWall
+            // 
+            this.pctRigtWall.BackgroundImage = global::TetrisTCP.Properties.Resources.duvar;
+            this.pctRigtWall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctRigtWall.Location = new System.Drawing.Point(1312, -2);
+            this.pctRigtWall.Name = "pctRigtWall";
+            this.pctRigtWall.Size = new System.Drawing.Size(24, 825);
+            this.pctRigtWall.TabIndex = 5;
+            this.pctRigtWall.TabStop = false;
+            // 
+            // tmrStart
+            // 
+            this.tmrStart.Interval = 1000;
+            this.tmrStart.Tick += new System.EventHandler(this.tmrStart_Tick);
+            // 
+            // tmrBall
+            // 
+            this.tmrBall.Interval = 50;
+            this.tmrBall.Tick += new System.EventHandler(this.tmrBall_Tick);
+            // 
+            // lblKeys
+            // 
+            this.lblKeys.AutoSize = true;
+            this.lblKeys.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKeys.ForeColor = System.Drawing.Color.Red;
+            this.lblKeys.Location = new System.Drawing.Point(87, 79);
+            this.lblKeys.Name = "lblKeys";
+            this.lblKeys.Size = new System.Drawing.Size(964, 46);
+            this.lblKeys.TabIndex = 6;
+            this.lblKeys.Text = "Player1- A-D , Player2 - LEFT,RIGHT ARROW KEYS";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 824);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblKeys);
+            this.Controls.Add(this.pctRigtWall);
+            this.Controls.Add(this.pctLeftWall);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.pctBall);
+            this.Controls.Add(this.pctPlayer2);
+            this.Controls.Add(this.pctPlayer1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tetrisk";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLeftWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRigtWall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctPlayer1;
+        private System.Windows.Forms.PictureBox pctPlayer2;
+        private System.Windows.Forms.PictureBox pctBall;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Timer tmrMovements;
+        private System.Windows.Forms.PictureBox pctLeftWall;
+        private System.Windows.Forms.PictureBox pctRigtWall;
+        private System.Windows.Forms.Timer tmrStart;
+        private System.Windows.Forms.Timer tmrBall;
+        private System.Windows.Forms.Label lblKeys;
+    }
+}
+
---
+++ TetrisTCP/TetrisTCP/frmMain.cs
@@ -0,0 +1,503 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TetrisTCP.Properties;
+
+namespace TetrisTCP
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {        
+            InitializeComponent();
+            player2X = pctPlayer2.Location.X;
+            player2Y = pctPlayer2.Location.Y;
+            player1X = pctPlayer1.Location.X;
+            player1Y = pctPlayer1.Location.Y;
+            ballX = pctBall.Location.X;
+            ballY = pctBall.Location.Y;
+
+
+        }
+        int player2X;
+        int player2Y;
+        int player1X;
+        int player1Y;
+        int ballX;
+        int ballY;
+        
+        Boolean player1Right;
+        Boolean player1Left;
+        Boolean player2Right;
+        Boolean player2Left;
+        Boolean rightWallHitBoolean;
+        Boolean leftWallHitBoolean;
+        Boolean scoreDone;
+        Random rnd = new Random();
+        Point after;
+        Point before;
+        int rndNew;
+        int sayac = 0;
+        Boolean player1Hit;
+        Boolean player2Hit;
+        int player1Score;
+        int player2Score;
+        SoundPlayer player;
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.A)
+            {
+                player1Left = true;
+              
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                player1Right =true;
+               
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                player2Right = true;
+               
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                player2Left = true;
+               
+            }
+
+        }
+
+        private void frmMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                player1Left = false;
+               
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                player1Right = false;
+              
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                player2Right = false;
+               
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                player2Left = false;
+               
+            }
+
+        }
+
+        private void tmrMovements_Tick(object sender, EventArgs e)
+        {
+            if (player1Right && player2Right)
+            {
+                if ((!(pctPlayer1.Location.X + 130 >= pctRigtWall.Location.X))&&(!(pctPlayer2.Location.X +130>=pctRigtWall.Location.X)))
+                {
+                    pctPlayer1.Location = new Point(pctPlayer1.Location.X + 8, pctPlayer1.Location.Y);
+                    pctPlayer2.Location = new Point(pctPlayer2.Location.X + 8, pctPlayer2.Location.Y);
+                }
+            }
+            if (player1Right && player2Left)
+            {
+                if ((!(pctPlayer1.Location.X + 130 >= pctRigtWall.Location.X))&&(  !(pctPlayer2.Location.X -28<= pctLeftWall.Location.X )))
+                {
+                pctPlayer1.Location = new Point(pctPlayer1.Location.X + 8, pctPlayer1.Location.Y);
+                pctPlayer2.Location = new Point(pctPlayer2.Location.X - 8, pctPlayer2.Location.Y);
+                }
+            }
+            if (player1Left && player2Left)
+            {
+                
+                if ((!(pctPlayer1.Location.X - 28 <= pctLeftWall.Location.X))&&(!(pctPlayer2.Location.X -28<= pctLeftWall.Location.X )))
+                {
+                pctPlayer1.Location = new Point(pctPlayer1.Location.X - 8, pctPlayer1.Location.Y);
+                pctPlayer2.Location = new Point(pctPlayer2.Location.X - 8, pctPlayer2.Location.Y);
+                }
+            }
+            if (player1Left && player2Right)
+            {
+                if ((!(pctPlayer1.Location.X - 28 <= pctLeftWall.Location.X)) && (!(pctPlayer1.Location.X + 130 >= pctRigtWall.Location.X)))
+                {
+                    pctPlayer1.Location = new Point(pctPlayer1.Location.X - 8, pctPlayer1.Location.Y);
+                    pctPlayer2.Location = new Point(pctPlayer2.Location.X + 8, pctPlayer2.Location.Y);
+                }
+            }
+            if (player1Left)
+            {
+
+                if (!(pctPlayer1.Location.X - 28 <= pctLeftWall.Location.X))
+                {
+                    pctPlayer1.Location = new Point(pctPlayer1.Location.X - 16, pctPlayer1.Location.Y);
+                }
+            }
+            if (player1Right)
+            {
+
+                if (!(pctPlayer1.Location.X + 130 >= pctRigtWall.Location.X))
+                {
+                    pctPlayer1.Location = new Point(pctPlayer1.Location.X + 16, pctPlayer1.Location.Y);
+                }
+            }
+            if (player2Right)
+            {
+
+                if (!(pctPlayer2.Location.X +130>=pctRigtWall.Location.X))
+                {
+                    pctPlayer2.Location = new Point(pctPlayer2.Location.X + 16, pctPlayer2.Location.Y);
+                }
+            }
+            if (player2Left)
+            {
+                if (!(pctPlayer2.Location.X -28<= pctLeftWall.Location.X ))
+                {
+                    pctPlayer2.Location = new Point(pctPlayer2.Location.X - 16, pctPlayer2.Location.Y);
+                }
+            }
+
+
+        }
+
+        private void tmrStart_Tick(object sender, EventArgs e)
+        {
+            if (scoreDone)
+            {
+                pctBall.Location = new Point(ballX, ballY);
+                pctPlayer1.Location = new Point(player1X, player1Y);
+                pctPlayer2.Location = new Point(player2X, player2Y);
+                rightWallHitBoolean = false;
+                leftWallHitBoolean = false;
+                player1Hit = false;
+                player2Hit = false;
+
+                if (sayac == 1)
+                {
+                    lblInfo.Text = "Scoreeeeee! Player1 :"+player1Score+" Player2 :" +player2Score;
+                    lblInfo.Show();
+
+                }
+                if (sayac == 2)
+                {
+                    lblInfo.Show();
+                }
+                if (sayac == 4)
+                {
+                    lblInfo.Text = "Game will be start in 3 seconds...";
+                 
+                }
+                if (sayac == 5)
+                {
+                    lblInfo.Text = "Game will be start in 2 seconds...";
+                   
+                }
+                if (sayac == 6)
+                {
+                    lblInfo.Text = "Game will be start in 1 seconds...";
+                 
+                }
+                if (sayac == 7)
+                {
+                    
+                    lblInfo.Hide();
+                    tmrMovements.Enabled = true;
+                    tmrStart.Enabled = false;
+                    rndNew = rnd.Next(1, 3);
+                    scoreDone = false;
+                    sayac = 0;
+                    startBall();
+                   
+                }
+                sayac++;
+                
+            }
+            else
+            {
+                if (sayac == 0)
+                {
+                    lblInfo.Text = "Game will be start in 3 seconds...";
+                }
+                if (sayac == 1)
+                {
+                    lblInfo.Text = "Game will be start in 2 seconds...";
+                }
+                if (sayac == 2)
+                {
+                    lblInfo.Text = "Game will be start in 1 seconds...";
+                }
+                if (sayac == 3)
+                {
+                    lblInfo.Hide();
+                    tmrMovements.Enabled = true;
+                    tmrStart.Enabled = false;
+                    rndNew = rnd.Next(1, 3);
+                    startBall();
+                    sayac = 0;
+                    lblKeys.Hide();
+                }
+                sayac++;
+               
+
+            }
+          
+         
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            tmrStart.Enabled = true; 
+        }
+        private void startBall()
+        {
+            rndNew = 1;
+            if (rndNew == 1)
+            {
+              before=pctBall.Location;
+              pctBall.Location = new Point(pctBall.Location.X, pctBall.Location.Y + 15);
+              after = new Point(pctBall.Location.X, pctBall.Location.Y + 15);
+            }
+            if (rndNew == 2)
+            {
+                before = pctBall.Location;
+                pctBall.Location = new Point(pctBall.Location.X, pctBall.Location.Y - 15);
+                after = new Point(pctBall.Location.X, pctBall.Location.Y - 15);
+            }
+          
+            tmrBall.Enabled = true;
+        }
+
+        private void tmrBall_Tick(object sender, EventArgs e)
+        {
+            goalChecks();
+            if (after.Y - before.Y > 0)
+            {
+
+                if ((pctBall.Location.X < pctPlayer2.Location.X + pctPlayer2.Size.Width && pctBall.Location.X > pctPlayer2.Location.X) && pctBall.Location.Y > pctPlayer2.Location.Y - pctPlayer2.Size.Height)
+                {
+                    player = new SoundPlayer(Resources.bounce);
+                    player.Play();
+                    player2Hit = true;
+                    player1Hit = false;
+                    player2HitRandom();
+
+                }
+                else if ( pctBall.Location.X+pctBall.Size.Width>=pctRigtWall.Location.X)
+                {
+                    player = new SoundPlayer(Resources.laser);
+                    player.Play();
+                    before = pctBall.Location;
+                    after = new Point(pctBall.Location.X-5, pctBall.Location.Y + 15);
+                    pctBall.Location = after;
+                    rightWallHitBoolean = true;
+                    leftWallHitBoolean = false;
+                    player2Hit = false;
+                    player1Hit = false;
+                    
+                }
+                else if (pctBall.Location.X <= pctLeftWall.Location.X)
+                {
+
+                    player = new SoundPlayer(Resources.laser);
+                    player.Play();
+                    before = pctBall.Location;
+                    after = new Point(pctBall.Location.X+5, pctBall.Location.Y +15);
+                    pctBall.Location = after;
+                    leftWallHitBoolean = true;
+                    rightWallHitBoolean = false;
+                    player1Hit = false;
+                    player2Hit = false;
+                }
+                else
+                {
+                    if (player1Hit)
+                    {
+                        before = pctBall.Location;
+                        if (rndNew == 1)
+                        {
+                            after = new Point(pctBall.Location.X + 5, pctBall.Location.Y + 15);
+                        }
+                        if (rndNew == 2)
+                        {
+                            after = new Point(pctBall.Location.X - 5, pctBall.Location.Y + 15);
+                        }
+                        pctBall.Location = after;
+                    }
+                    if (!player1Hit && !player2Hit&&!rightWallHitBoolean&&!leftWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X, pctBall.Location.Y + 15);
+                        pctBall.Location = after;
+                    }
+                    else if (rightWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X - 5, pctBall.Location.Y + 15);
+                        pctBall.Location = after;
+                    }
+                    else if (leftWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X + 5, pctBall.Location.Y + 15);
+                        pctBall.Location = after;
+                    }
+                }
+            }
+            if (after.Y - before.Y < 0)
+            {
+                if ((pctBall.Location.X < pctPlayer1.Location.X + pctPlayer1.Size.Width && pctBall.Location.X > pctPlayer1.Location.X)  && pctBall.Location.Y < pctPlayer1.Location.Y + pctPlayer1.Size.Height)
+                {
+                    player = new SoundPlayer(Resources.bounce);
+                    player.Play();
+                    player1Hit = true;
+                    player2Hit = false;
+                    player1HitRandom();
+                    
+                }
+                else if (pctBall.Location.X+pctBall.Size.Width >= pctRigtWall.Location.X)
+                {
+
+                    player = new SoundPlayer(Resources.laser);
+                    player.Play();
+                    before = pctBall.Location;
+                    after = new Point(pctBall.Location.X-5, pctBall.Location.Y - 15);
+                    pctBall.Location = after;
+                    rightWallHitBoolean = true;        
+                    leftWallHitBoolean = false;
+                    player1Hit = false;
+                    player2Hit = false;
+                }
+                else if (pctBall.Location.X <= pctLeftWall.Location.X)
+                {
+
+                    player = new SoundPlayer(Resources.laser);
+                    player.Play();
+                    before = pctBall.Location;
+                    after = new Point(-pctBall.Location.X+5, pctBall.Location.Y - 15);
+                    pctBall.Location = after;
+                    leftWallHitBoolean = true;
+                    rightWallHitBoolean = false;
+                    player1Hit = false;
+                    player2Hit = false;
+                }
+                else
+                {
+                    if (player2Hit)
+                    {
+                        before = pctBall.Location;
+                        if (rndNew == 1)
+                        {
+                            after = new Point(pctBall.Location.X + 5, pctBall.Location.Y - 15);
+                        }
+                        if (rndNew == 2)
+                        {
+                            after = new Point(pctBall.Location.X - 5, pctBall.Location.Y - 15);
+                        }
+                            pctBall.Location = after;
+                    }
+                    if(!player1Hit && !player2Hit&&!rightWallHitBoolean&&!leftWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X, pctBall.Location.Y - 15);
+                        pctBall.Location = after;
+                    }
+                    else if (rightWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X - 5, pctBall.Location.Y - 15);
+                        pctBall.Location = after;
+                    }
+                    else if (leftWallHitBoolean)
+                    {
+                        before = pctBall.Location;
+                        after = new Point(pctBall.Location.X + 5, pctBall.Location.Y - 15);
+                        pctBall.Location = after;
+                    }
+                }       
+            }
+        }
+        private void leftWallHit()
+        {
+           
+
+        }
+        private void player1HitRandom()
+        {
+            rndNew = rnd.Next(1, 3);
+            if (rndNew == 1)
+            {
+
+                before = pctBall.Location;
+                after = new Point(pctBall.Location.X+5, pctBall.Location.Y + 15);
+                pctBall.Location = after;
+            }
+            if (rndNew == 2)
+            {
+                before = pctBall.Location;
+                after = new Point(pctBall.Location.X - 5, pctBall.Location.Y + 15);
+                pctBall.Location = after;
+            }
+        }
+        private void player2HitRandom()
+        {
+            
+            rndNew = rnd.Next(1, 3);
+            if (rndNew == 1)
+            {
+
+                before = pctBall.Location;
+                after = new Point(pctBall.Location.X+5, pctBall.Location.Y - 15);
+                pctBall.Location = after;
+            }
+            if (rndNew == 2)
+            {
+                before = pctBall.Location;
+                after = new Point(pctBall.Location.X - 5, pctBall.Location.Y - 15);
+                pctBall.Location = after;
+            }
+        }
+        private void goalChecks()
+        {
+            if (pctBall.Location.Y+pctBall.Size.Height > 825)
+            {
+                player = new SoundPlayer(Resources.goal);
+                player.Play();
+                scoreDone = true;
+                player2Score++;
+                tmrBall.Enabled = false;
+                tmrMovements.Enabled = false;
+                tmrStart.Enabled = true;
+               
+                
+                
+            }
+
+            if (pctBall.Location.Y < -2)
+            {
+                player = new SoundPlayer(Resources.goal);
+                player.Play();
+                scoreDone = true;
+                player1Score++;
+                tmrBall.Enabled = false;
+                tmrMovements.Enabled = false;
+                tmrStart.Enabled = true;
+            }
+            
+           
+           
+           
+        }
+    }
+    
+}
